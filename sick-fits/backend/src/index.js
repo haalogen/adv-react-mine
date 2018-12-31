f6a9506c3c04912646a8e73cc71388542b868340
Stanislav Nikitin <nikitin.develop@gmail.com>
2019-01-01 02:19:53 +0300

    12 Setup GraphQL Yoga Server

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,19 @@
-// let's go!
+// This file starts up our Node.js server
+require('dotenv').config({ path: 'variables.env' })
+const createServer = require('./createServer')
+const db = require('./db')
+
+const server = createServer()
+
+// TODO: Use Express middleware to handle cookies (JWT)
+// TODO: Use Express middleware to populate current user
+
+server.start({
+  // We want this endpoint to be visited by only approved URLs
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  }
+}, details => {
+  console.log(`Server is now running on http://localhost:${details.port}`)
+})
