f13dface0393c38e183ef941e168a3cf84692beb
Stanislav Nikitin <nikitin.develop@gmail.com>
2019-01-01 20:16:56 +0300

    15 Setup Apollo Client with React

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,18 +1,38 @@
 import React from 'react'
 import App, {Container} from 'next/app'
+import { ApolloProvider } from 'react-apollo'
 import Page from '../components/Page'
+import withData from '../lib/withData'
 
 class MyApp extends App {
+  /**
+   * Next.js special lifecycle method. Runs before first render() runs.
+   * Initializes the props.
+   */
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {}
+    // Crawl any page for all the Queries and Mutations inside of that page
+    // Fire off them all and resolve before we render the page
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+    // This exposes URL query to the user
+    pageProps.query = ctx.query
+    return { pageProps }
+  }
+
   render() {
-    const { Component } = this.props;
+    const { apollo, Component, pageProps } = this.props;
     return (
       <Container>
-        <Page>
-          <Component/>
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     )
   }
 }
 
-export default MyApp
+export default withData(MyApp)
