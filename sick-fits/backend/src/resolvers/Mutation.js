e0a5fbc541b435a2b8ea9fa0622435c9d9d36852
Stanislav Nikitin <nikitin.develop@gmail.com>
2019-01-01 14:11:13 +0300

    13 Write 1st Query and Mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,13 @@
-const Mutation = {};
+const Mutation = {
+  createDog(parent, args, ctx, info) {
+    console.log(args);
+    // Get dogs
+    global.dogs = global.dogs || []
+    // Create a dog!
+    const newDog = { name: args.name }
+    global.dogs.push(newDog)
+    return newDog
+  },
+};
 
 module.exports = Mutation;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,11 @@
-const Query = {};
+const Query = {
+  // Shorthand syntax for
+  // dogs: function () {}
+  dogs(parent, args, ctx, info) {
+    // Get dogs
+    global.dogs = global.dogs || []
+    return global.dogs
+  },
+};
 
 module.exports = Query;
