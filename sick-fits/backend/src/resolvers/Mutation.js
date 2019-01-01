e76cc33410161e10e286cec0a46baed631bcd8e1
Stanislav Nikitin <nikitin.develop@gmail.com>
2019-01-01 16:55:22 +0300

    14 Items creation. Prisma Yoga flow

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,13 +1,27 @@
 const Mutation = {
-  createDog(parent, args, ctx, info) {
-    console.log(args);
-    // Get dogs
-    global.dogs = global.dogs || []
-    // Create a dog!
-    const newDog = { name: args.name }
-    global.dogs.push(newDog)
-    return newDog
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        // title: args.title, etc
+        ...args,
+      }
+    }, info) /* "info" says what to return as a result */
+
+    // console.log(item)
+
+    return item
   },
+  // createDog(parent, args, ctx, info) {
+  //   console.log(args);
+  //   // Get dogs
+  //   global.dogs = global.dogs || []
+  //   // Create a dog!
+  //   const newDog = { name: args.name }
+  //   global.dogs.push(newDog)
+  //   return newDog
+  // },
 };
 
-module.exports = Mutation;
+module.exports = Mutation
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,11 +1,23 @@
+const { forwardTo } = require('prisma-binding')
+
+
 const Query = {
-  // Shorthand syntax for
-  // dogs: function () {}
-  dogs(parent, args, ctx, info) {
-    // Get dogs
-    global.dogs = global.dogs || []
-    return global.dogs
-  },
+  // Don't add any custom logic (auth, charge card, send email, etc).
+  // Pass the query through to database
+  items: forwardTo('db'),
+
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items()
+  //   return items
+  // },
+
+  // // Shorthand syntax for
+  // // dogs: function () {}
+  // dogs(parent, args, ctx, info) {
+  //   // Get dogs
+  //   global.dogs = global.dogs || []
+  //   return global.dogs
+  // },
 };
 
-module.exports = Query;
+module.exports = Query
